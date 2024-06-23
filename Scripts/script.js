a002bc7eaae5e8b6dd999e3255e09b3f934ce51f
David Higgins <david14higgins@gmail.com>
2024-06-23 13:32:31 +0100

    smooth scrolling links

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,16 @@
 
 //Responsive Navbar
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-})
+});
+
+$('a[href^="#"]').not('a[href="#"]').click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+});
+
