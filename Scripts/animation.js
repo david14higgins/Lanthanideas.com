21a2d20c91db5529b0d1d47cdebe2fd0d8dbe432
David Higgins <david14higgins@gmail.com>
2024-06-26 12:55:59 +0100

    refactor + commenting

--- Scripts/animation.js
+++ Scripts/animation.js
@@ -96,7 +96,7 @@ let tl = gsap.timeline({defaults: {ease: "power4.inOut", duration: 2}});
 
 tl.to('#landing-page h1', {'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', opacity: 1, y: 0, duration: 2.2})
   .to('#landing-page h2', {'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', opacity: 1, y: 0}, "-=2")
-  .to('#profile-pic-container', {y: 0, opacity: 1}, "-=2")
+  .to('#landing-pic-container', {y: 0, opacity: 1}, "-=2")
   .to('#learn-more-container', {opacity: 1}, "-=1.5")
   .to('.navbar', {y: 0}, '-=3')
   .to('.logo', {opacity: 1}, '-=1.5')
