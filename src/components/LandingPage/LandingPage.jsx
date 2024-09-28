53bf7125081909c8c6a9f64faa4ea974a7aacd9f
David Higgins <david14higgins@gmail.com>
2024-09-28 23:29:52 +0100

    update landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import profilePicture from "../../assets/images/Profile-Picture.jpeg"
 import './LandingPage.css'
 import BackgroundLogo from '../Background/BackgroundLogo'
 import { Element, Link } from "react-scroll";
@@ -8,15 +7,12 @@ const LandingPage = () => {
   return (
     <Element name="langing-page">
       <div id="landing-page">
-          <div className="landing-content">
+          <div className="landing-content"> 
               <h1>Welcome to Lanthanideas</h1>
-              <h2>By Ian Higgins, <span className="coral-highlight">Consultant</span> to the Rare Earth Industry</h2>
-              <div className="landing-pic-container"> 
-                  <img src={profilePicture}/>
-              </div>
+              <h2><span className="highlight">Consultancy</span> to the Rare Earth Industry</h2>
               <div className="learn-more-btn-container">
                 <Link to="about-page" smooth={true} duration={1000}>Learn More</Link>
-              </div>
+              </div>  
           </div>  
           <BackgroundLogo position="upper"/>
           <BackgroundLogo position="lower"/>
